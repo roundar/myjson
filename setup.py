8509cc029bb7efe8391ea229af2bf40bbc8784df
roundar <roundar.github@gmail.com>
2017-06-16 09:01:30 -0400

    Converted readme to .rst

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
+import os
 from distutils.core import setup
+
 
 setup(
     name='myjson',
-    version='1.1.0',
+    version="1.1.1",
     description='API wrapper for myjson',
+    long_description=open(os.path.join(os.path.dirname(__file__), "readme.rst")).read(),
     url='https://github.com/roundar/myjson',
-    download_url='https://github.com/roundar/myjson/archive/v1.1.0.tar.gz',
+    download_url='https://github.com/roundar/myjson/archive/v1.1.1.tar.gz',
     packages=['myjson'],
     license='WTFPL',
     author='roundar',
